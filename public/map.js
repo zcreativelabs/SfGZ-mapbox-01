8b11f496061cca8c983a5ad305b2e5f6d37821f2
zimrick <richard@zcreativelabs.com>
2021-03-25 18:04:06 +0100

    Initial commit

---
+++ public/map.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  //
+  // 1. Register token
+  //
+
+  //
+  // 2. Initialize map
+  //
+
+  //
+  // 3. Once the map is loaded...
+  //
+
+    //
+    // 4. Fetch the fountain data
+    //
+
+        //
+        // 5. Add fountains as a geojson layer of points
+        //
+
+        //
+        // 6. Add tooltips
+        //
+
+})
